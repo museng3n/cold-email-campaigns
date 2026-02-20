08c534cffb3f8e93861d2b4267675ca03f6b93ee
museng3n <m2jengnnn@gmail.com>
2026-02-20 11:18:47 +0300

    GHL-53: read token from URL param

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
+"use client"
+
+import { useEffect } from "react"
 import CampaignsPage from "@/components/campaigns-page"
 
 export default function Home() {
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search)
+    const urlToken = urlParams.get('token')
+    if (urlToken) {
+      localStorage.setItem('authToken', urlToken)
+      window.history.replaceState({}, '', window.location.pathname)
+    }
+  }, [])
+
   return <CampaignsPage />
 }
